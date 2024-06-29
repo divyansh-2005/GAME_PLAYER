04b5df30aa6a9d62f52b6679fcc0aece0bae7769
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-06-29 11:00:12 +0530

    user

--- my-project/src/Components/HeroSection.jsx
+++ my-project/src/Components/HeroSection.jsx
@@ -6,11 +6,13 @@ import { Me } from './tma/me';
 const HeroSection = () => (
   <section className="comp-section" id="home">
     <div className="compcontainer">
-      <h1 className='herosectionH1'>TeleGames</h1>
-      <h2>
-        <TmaProvider>
+      <h1 className='herosectionH1'>
+      <TmaProvider>
           <Me />
         </TmaProvider>
+      </h1>
+      <h2>
+        
       </h2>
       <h3>Instant Games, spot in a single place!!</h3>
       <p className="paragraph">
