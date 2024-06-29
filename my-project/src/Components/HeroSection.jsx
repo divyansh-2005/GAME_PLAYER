8a222693cf2ed93812a356bdb7a80fd72846e4aa
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-06-29 10:57:03 +0530

    username

--- my-project/src/Components/HeroSection.jsx
+++ my-project/src/Components/HeroSection.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import { TmaProvider } from './tma/provider';
+import { Me } from './tma/me';
 
 const HeroSection = () => (
   <section className="comp-section" id="home">
     <div className="compcontainer">
       <h1 className='herosectionH1'>TeleGames</h1>
+      <h2>
+        <TmaProvider>
+          <Me />
+        </TmaProvider>
+      </h2>
       <h3>Instant Games, spot in a single place!!</h3>
       <p className="paragraph">
         TeleGames is a collection of instant Games
