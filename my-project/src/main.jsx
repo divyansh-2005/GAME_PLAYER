c2d1359dd02fc65d8aaaa95a8eb46d705afd96dd
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-06-30 14:16:34 +0530

    plz

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { TmaProvider } from './Components/tma/provider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TmaProvider>
+      <App />
+    </TmaProvider>
   </React.StrictMode>,
 )
