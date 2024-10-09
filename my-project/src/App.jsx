b1222c3030ea6cc9f77e6bfd0a95723c71ced128
Bhavy_Zala <zalabhavy2004@gmail.com>
2024-10-09 22:51:34 +0530

    Git new add

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -7,14 +7,13 @@ import GTH from "./Games/GuessTheColor/GTH";
 import PongGame from "./Games/PingPongGame/PongGame";
 import EmojiIntruderHunt from "./Games/EmojiIntruder/EmojiIntruder";
 import RockPaperScissors from "./Games/RockPaper Scissors/Game";
-import GameSection from "./Components/GameSection";
 import Dashboard from "./Components/Dashboard";
 import AddGamePage from "./Components/AddGamePage";
 import { useTma } from "./Context/tmaProvider";
 import Games from "./Components/Games";
 import AboutPage from "./Components/AboutPage";
 import ContactPage from "./Components/ContactPage";
-import TetrisGame from "./Games/Tetris/TetrisGame";
+
 
 const App = () => {
   const { user, isLoading, isError } = useTma();
@@ -31,7 +30,6 @@ const App = () => {
         <Route path="/emojiintruder" element={<EmojiIntruderHunt />} />
         <Route path="/rckpapsc" element={<RockPaperScissors />} />
         <Route path="/PingPong" element={<PongGame/>} />
-        <Route path="/tetris" element={<TetrisGame />} /> 
         <Route path="/addgame" element={<AddGamePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
