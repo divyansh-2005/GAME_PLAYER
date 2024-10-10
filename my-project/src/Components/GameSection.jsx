a287c30ef03abe6eba00cc876f3697c05b69623d
Mehul <mehulprajapati2802@gmail.com>
2024-10-10 12:48:41 -0400

    fix doc string

--- my-project/src/Components/GameSection.jsx
+++ my-project/src/Components/GameSection.jsx
@@ -53,7 +53,7 @@ const GameSection = () => {
     {
         name: "Arkanoid",
         description:
-          "Controlling a paddle-like craft known as the Vaus, the player is tasked with clearing a formation of colorful blocks by deflecting a ball towards it without letting the ball leave the bottom edge of the playfield.",
+          "Controlling a paddle-like craft known as the Vaus, the player is tasked with clearing a formation of colorful blocks by deflecting a ball towards it.",
         link: "/arkanoid",
     },
   ]);
