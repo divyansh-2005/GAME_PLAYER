4517275bb0a89485dc10267e1b771e5ed270cf3f
Rudra Pratap Singh <130541940+rudrapratap63@users.noreply.github.com>
2024-10-15 18:36:42 +0530

    Add slot machine game

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -25,6 +25,7 @@ import ColorMemoGame from "./Games/Color-memoGame/ColorMemoGame";
 import AimShooterGame from "./Games/Aim-Shooter-Game/AimShooterGame";
 import CandyCrushGame from "./Games/Candy-crush-saga/src/CandyCrushGame";
 import DiceGameTenzi from "./Games/Dice-Game-Tenzi/src/DiceGameTenzi";
+import SlotMachineGame from "./Games/Slot-Machine-Game/src/SlotMachineGame";
 
 const App = () => {
   const { user, isLoading, isError } = useTma();
@@ -60,6 +61,7 @@ const App = () => {
         <Route path="/aimshooter" element={<AimShooterGame />} />
         <Route path="/candycrush" element={<CandyCrushGame />} />
         <Route path="/dicegame" element={<DiceGameTenzi />} />
+        <Route path="/slot-machine-game" element={<SlotMachineGame />} />
       </Routes>
     </>
   );
