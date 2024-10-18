cefbac999f265b2e5fb276a9d528be335c038ba6
samriddhitiwary <151003533+samriddhitiwary@users.noreply.github.com>
2024-10-18 21:23:51 +0530

    Connect Four

--- my-project/src/Components/GameSection.jsx
+++ my-project/src/Components/GameSection.jsx
@@ -133,6 +133,12 @@ const GameSection = () => {
           "The Tower of Hanoi is a classic puzzle game that challenges players to move a stack of disks from one rod to another, following specific rules.",
         link: "/towerofhanoi",
       },
+      {
+        name: "Connect Four ",
+        description:
+          "The Tower of Hanoi is a classic puzzle game that challenges players to move a stack of disks from one rod to another, following specific rules.",
+        link: "/connectFour",
+      },
   ]);
 
   const [searchTerm, setSearchTerm] = useState("");
