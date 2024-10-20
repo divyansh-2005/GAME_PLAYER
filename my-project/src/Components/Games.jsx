ab172449a3b5cd1e9c9e9f4086a7fa5e408d5582
Raj Fauzdar <121241266+rajfauzdar@users.noreply.github.com>
2024-10-21 00:14:01 +0530

    Added Footer and scroll to top button on Games Page

--- my-project/src/Components/Games.jsx
+++ my-project/src/Components/Games.jsx
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import GameSection from './GameSection'
 import Header from './Header'
+import Footer from './Footer'
+import ScrollToTopButton from './ScrollToTopButton'
+
+
 
 function Games() {
+  
+  const calcScrollValue = () => {
+    const scrollProgress = document.getElementById('progress');
+    const pos = document.documentElement.scrollTop;
+    const calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollValue = Math.round((pos * 100) / calcHeight);
+  
+    if (pos > 100) {
+      scrollProgress.style.display = 'grid';
+    } else {
+      scrollProgress.style.display = 'none';
+    }
+  
+    scrollProgress.addEventListener('click', () => {
+      document.documentElement.scrollTop = 0;
+    });
+  
+    scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+  };
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      document.body.classList.add('loaded');
+    }, 500);
+
+    window.addEventListener('scroll', calcScrollValue);
+
+    return () => {
+      clearTimeout(timer);
+      window.removeEventListener('scroll', calcScrollValue);
+    };
+  }, []);
+
   return (
     <>
     <Header/>
     <GameSection/>
+    <ScrollToTopButton />
+    <Footer/>
     </>
   )
 }
