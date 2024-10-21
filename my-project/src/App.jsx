7ae6a49e7b158b9acc895dd8103d438ba0524cf8
Krish Chothani <143370415+KrishChothani@users.noreply.github.com>
2024-10-21 22:40:37 +0530

    try to resolve conflict

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -35,6 +35,7 @@ import WriteTypingGame from "./Games/Write-Typing-Game/src/WriteTypingGame"
 import TowerOfHanoi from "./Games/TowerOfHanoi/TowerOfHanoi";
 import DuckHunt from "./Games/DuckHunt/DuckHunt";
 import AnimatedCursor from "react-animated-cursor"
+import RealmRush from "./Games/RealmRush/RealmRush"
 
 const App = () => {
   const { user, isLoading, isError } = useTma();
